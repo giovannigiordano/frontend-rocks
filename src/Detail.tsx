24319fe663fa086ad2d6f70ee458ded80bb84264
Giovanni Giordano <giordano.giovanni1993@gmail.com>
2026-01-15 13:22:14 +0000

    Prima versione

--- src/Detail.tsx
+++ src/Detail.tsx
@@ -1,3 +1,10 @@
 export const Detail = () => {
-  return null;
+  return <div className="text-5xl space-x-2 space-y-2 flex flex-wrap justify-end">
+    <Card title="01" />
+    <Card title="02 " />
+  </div>
 };
+
+const Card = (props: { title: string }) => {
+  return <div className="bg-green-500 w-40 h-40 text-center flex items-center justify-center" title={props.title}>{props.title}</div>
+}
--- src/Root.tsx
+++ src/Root.tsx
@@ -1,48 +1,96 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router";
+import { PokeAPI } from "./api";
 
-export const Root = () => {
-  const [count, setCount] = useState(0);
-  const [title, setTitle] = useState("Titolo iniziale");
 
-  useEffect(() => {
-    if (count === 4) {
-      setTitle("Il titolo ha superato il 4");
-    }
-  }, [count]);
+interface Props {
+  id: number;
+  image: string;
+  name: string;
+  types: string[];
+}
 
-  return (
-    <div className="h-dvh flex flex-col items-center justify-center">
-      <div className="bg-white p-8 rounded-md shadow-lg">
-        <h1 className="text-center font-bold text-3xl text-blue-400 mb-4">
-          {title}
-        </h1>
+export const Card: React.FC<Props> = (props) => (
+  <div className="bg-white border border-gray-300 rounded-lg shadow-md relative w-2xs flex items-center justify-center h-80">
+    <h4 className="text-xl text-gray-900 tracking-wide font-bold absolute left-4 top-2">
+      {props.name} -{" "}
+      <span className="text-gray-700 font-medium">{props.id}</span>
+    </h4>
 
-        <h2 className="text-center font-bold text-xl mb-6">Vite + React</h2>
+    <img
+      src={props.image}
+      alt={props.name}
+      className="w-36 h-36 object-contain"
+    />
 
-        <div className="flex flex-col items-center space-y-4">
-          <button
-            className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-blue-600 transition-colors"
-            onClick={() => setCount((count) => count + 1)}
+    <div className="text-sm text-gray-700 absolute right-2 bottom-2">
+      <div className="flex justify-center space-x-2">
+        {props.types.map((type) => (
+          <span
+            key={type}
+            className={`font-bold text-white px-3 py-1 rounded-md text-xs ${getTypeColor(type)}`}
           >
-            Hai premuto il pulsante {count} {count === 1 ? "volta" : "volte"}
-          </button>
-
-          <button
-            className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-blue-600 transition-colors"
-            onClick={() => setTitle("Charizard")}
-          >
-            Cambia titolo
-          </button>
-
-          <Link
-            to="/frontend-rocks/dettaglio/1"
-            className="text-blue-500 underline"
-          >
-            Link alla pagina di dettaglio
-          </Link>
-        </div>
+            {type}
+          </span>
+        ))}
       </div>
     </div>
-  );
+  </div>
+);
+export function Root() {
+  return <Card
+    id={0}
+    image="https://placeholdit.com/400/dddddd/999999"
+    name="Pikachu"
+    types={["grass"]}
+    />
+}
+function getTypeColor(type: string): string {
+  return typeColors[type];
+}
+
+const typeColors: { [key: string]: string } = {
+  fire: "bg-red-500",
+  water: "bg-blue-500",
+  grass: "bg-green-500",
+  electric: "bg-yellow-400",
+  psychic: "bg-pink-500",
+  ice: "bg-cyan-400",
+  dragon: "bg-purple-700",
+  dark: "bg-gray-700",
+  fairy: "bg-pink-300",
+  normal: "bg-gray-400",
+  fighting: "bg-red-700",
+  flying: "bg-indigo-400",
+  poison: "bg-purple-500",
+  ground: "bg-yellow-600",
+  rock: "bg-yellow-800",
+  bug: "bg-green-700",
+  ghost: "bg-indigo-700",
+  steel: "bg-gray-500",
 };
+
+
+interface PokemonCard {
+  id: number;
+  image: string;
+  name: string;
+  types: string[];
+}
+/*
+async function fetchData(offset: number): Promise<PokemonCard[]> {
+  const list = await PokeAPI.listPokemons(offset, 20);
+  const pokemons = await Promise.all(
+    list.results.map(async (item: { name: string; url: string }) => {
+      const pokemon = await PokeAPI.getPokemonByName(item.name);
+      return pokemon;
+    }),
+  );
+
+  return pokemons.map((item) => ({
+    id: item.id,
+    image: item.sprites.other?.["official-artwork"].front_default ?? "",
+    name: item.name,
+    types: item.types.map((type) => type.type.name),
+  }));
+}*/
